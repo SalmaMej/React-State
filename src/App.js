6f1ec1e403cffdd0577dcd2343020f94f2bec566
SalmaMej <salma.mejdouba@gmail.com>
2020-12-09 10:47:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import './components/setInterval'
+import Timer from './components/setInterval';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  
+    state = {
+    fullName : 'Salma Ben Mejdouba',
+    bio : 'Interior designer and JS full stack developer.',
+    imgSrc : 'avatar-2.png',
+    show : false,
+  };
+
+  Active = () => {
+    this.setState({
+      show : !this.state.show
+    })
+  }
+
+  render() {
+  return(
+    <div className = 'App'>
+  <button onClick = {this.Active}>Show</button>
+  {this.state.show ? 
+  (<div>
+  <h1>{this.state.fullName}</h1>
+  <p>{this.state.bio}</p>
+  <img src = {this.state.imgSrc} alt = 'profile' />
+  </div>) : (<p>Press the button!</p>)
+  }
+  <Timer />
+  
+  </div>
+  )
+  }
+
+  
 }
+
 
 export default App;
